abf904d004835a07fb437262fd6acb7060375b03
Peter Malkin <github.com/PeterMalkin>
2021-04-28 15:09:16 -0700

    Add KaiData hourly fx dataset for backtesting

--- backtest.py
+++ backtest.py
@@ -1,7 +1,7 @@
 import backtrader
+import datetime
 import importlib
 import settings
-
 
 class HistDataCSVData(backtrader.feeds.GenericCSVData):
     params = (
@@ -20,6 +20,42 @@ class HistDataCSVData(backtrader.feeds.GenericCSVData):
         ('separator', ';'),
     )
 
+class KaiDataCSVData(backtrader.feeds.GenericCSVData):
+    params = (
+        ('nullvalue', float('NaN')),
+        ('dtformat', 1),
+        ('headers', True),
+        ('time', -1),
+        ('datetime', 0),
+        ('open', 1),
+        ('close', 2),
+        ('high', 3),
+        ('low', 4),
+        ('volume', 5),
+        ('openinterest', -1),
+        ('reverse', True),
+    )
+
+def load_backtest_data():
+
+    data = None
+
+    if settings.BACKTESTING_FORMAT == "KAIDATA":
+        data = KaiDataCSVData(
+            dataname=settings.BACKTESTING_FILENAME,
+            separator='\t',
+            timeframe=backtrader.TimeFrame.Minutes,
+            compression=60,
+            fromdate=datetime.datetime(2008,1,1),
+            todate=datetime.datetime(2008,6, 1))
+
+    if settings.BACKTESTING_FORMAT == "HISTDATA":
+        data = HistDataCSVData(
+            dataname=settings.BACKTESTING_FILENAME,
+            dtformat="%Y%m%d %H%M%S")
+
+    return data
+
 
 def strategy_class(class_name):
     module = importlib.import_module("strategy")
@@ -28,7 +64,7 @@ def strategy_class(class_name):
                 
 def backtest():
     cerebro = backtrader.Cerebro()
-    data = HistDataCSVData(dataname=settings.BACKTESTING_FILENAME,dtformat="%Y%m%d %H%M%S")
+    data = load_backtest_data()
     cerebro.adddata(data)
     cerebro.addstrategy(strategy_class(settings.STRATEGY_NAME))
     cerebro.broker.setcommission(commission=0.001)
